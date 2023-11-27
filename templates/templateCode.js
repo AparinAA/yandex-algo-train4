bd35a6836787c24e7ba1860e450d009bc74b3bee
Александр Апарин <aparinalx@yandex.ru>
2023-11-27 16:04:00 +0100

    init

--- templates/templateCode.js
+++
@@ -1,9 +0,0 @@
-function solution(data) {
-	// parse data
-	// let [...] = data.trim().split(" ");
-	//to do something, here code
-}
-const fs = require("fs");
-const file = fs.readFileSync("input.txt", "utf8").trim();
-result = solution(file);
-fs.writeFileSync("output.txt", `${result}`);
